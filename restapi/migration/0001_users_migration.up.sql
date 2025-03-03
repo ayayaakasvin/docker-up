841bf3d9ba32ad92d75a371758ecf323fd28c555
Nurzhalgas <n_kozhamseit@kbtu.kz>
2025-03-03 13:44:32 +0500

    update

--- restapi/migration/0001_users_migration.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL, -- Hashed password
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
